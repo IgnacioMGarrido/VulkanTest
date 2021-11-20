d4f83d2fc1d0527ecf3bcc1e8b60d8e928d9dbf7
Nacho <nama1510@gmail.com>
2021-11-20 17:18:17 +0100

    Vulkan Instnce setup

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
@@ -16,7 +17,6 @@ public:
         mainLoop();
         cleanup();
     }
-    GLFWwindow* window;
 
 private:
     
@@ -30,7 +30,7 @@ private:
     
     void initVulkan() 
     {
-
+        createInstance();
     }
 
     void mainLoop() 
@@ -43,9 +43,58 @@ private:
 
     void cleanup() 
     {
+        //Destroy Vulkan Context
+        vkDestroyInstance(vkInstance, nullptr);
+
+        //Destroy glfw context
         glfwDestroyWindow(window);
         glfwTerminate();
     }
+
+    void createInstance() 
+    {
+        //Fill Info about the instance
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        //Tell driver about glbal extensions and validation layers
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        //Get the number of extensions
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        //Query extensions details
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        if (vkCreateInstance(&createInfo, nullptr, &vkInstance) != VK_SUCCESS) 
+        {
+            throw std::runtime_error("Failed to create Vulkan Instance");
+        }
+
+    }
+
+private:
+    GLFWwindow* window;
+    VkInstance vkInstance;
 };
 
 int main() {
