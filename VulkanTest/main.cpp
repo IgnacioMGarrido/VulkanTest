7e17a70fcc86a9e9fdfa1faf3063e1d6c46a53cb
Nacho <nama1510@gmail.com>
2021-11-21 17:55:07 +0100

    Logical Devices and Queues

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -122,6 +122,49 @@ private:
         setupMessenger();
 
         pickPhysicalDevice();
+
+        createLogicalDevice();
+    }
+
+    void createLogicalDevice() 
+    {
+        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+        VkDeviceQueueCreateInfo queueCreateInfo{};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+        queueCreateInfo.queueCount = 1;
+
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+
+        VkPhysicalDeviceFeatures devicefeatures{};
+
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pQueueCreateInfos = &queueCreateInfo;
+        createInfo.queueCreateInfoCount = 1;
+        createInfo.pEnabledFeatures = &devicefeatures;
+
+        createInfo.enabledExtensionCount = 0;
+
+        if (enableValidationLayers) 
+        {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        }
+        else 
+        {
+            createInfo.enabledLayerCount = 0;
+        }
+
+
+        if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create logical device!");
+        }
+
+        vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
     }
 
     void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo)
@@ -164,6 +207,9 @@ private:
         {
             DestroyDebugUtilsMessengerEXT(vkInstance, debugMessenger, nullptr);
         }
+
+        vkDestroyDevice(device, nullptr);
+
 
         //Destroy Vulkan Context
         vkDestroyInstance(vkInstance, nullptr);
@@ -318,6 +364,8 @@ private:
 
 private:
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkQueue graphicsQueue;
+    VkDevice device;
     GLFWwindow* window;
     VkInstance vkInstance;
     VkDebugUtilsMessengerEXT debugMessenger;
