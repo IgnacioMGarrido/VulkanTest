affae3bc2bc4cd1b702f1b99634d712adacd0f45
Nacho <nama1510@gmail.com>
2021-12-14 17:19:25 +0100

    Push Commands

--- VulkanTest/Application.cpp
+++ VulkanTest/Application.cpp
@@ -1,8 +1,18 @@
 #include "Application.h"
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_ONE
+#include <glm/glm.hpp>
+
 #include <stdexcept>
 #include <array>
 namespace VE 
 {
+    struct SimplePushConstantData //Temporary
+    {
+        glm::vec2 offset;
+        alignas(16) glm::vec3 color;
+    };
+
     Application::Application()
     {
         LoadModels();
@@ -39,12 +49,17 @@ namespace VE
 
     void Application::CreatePipelineLayout()
     {
+        VkPushConstantRange pushConstantRange;
+        pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+        pushConstantRange.offset = 0;
+        pushConstantRange.size = sizeof(SimplePushConstantData);
+
         VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
         pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
         pipelineLayoutInfo.setLayoutCount = 0;
         pipelineLayoutInfo.pSetLayouts = nullptr;
-        pipelineLayoutInfo.pushConstantRangeCount = 0;
-        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 1;
+        pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
 
         if (vkCreatePipelineLayout(m_device.device(), &pipelineLayoutInfo, nullptr, &m_pipelineLayout) != VK_SUCCESS)
         {
@@ -145,6 +160,8 @@ namespace VE
 
     void Application::RecordCommandBuffer(int imageIndex)
     {
+        static int frame = 0;
+        frame = (frame + 1) % 10000;
         VkCommandBufferBeginInfo beginInfo{};
         beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -163,7 +180,7 @@ namespace VE
         renderPassInfo.renderArea.extent = m_SwapChain->getSwapChainExtent();
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = { 0.1f, 0.1f, 0.1f, 0.1f };
+        clearValues[0].color = { 0.01f, 0.01f, 0.01f, 0.01f };
         clearValues[1].depthStencil = { 1.0f, 0 };
 
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
@@ -187,7 +204,18 @@ namespace VE
 
         m_pipeline->Bind(m_commandBuffers[imageIndex]); //Bind Graphics pipeline
         m_model->Bind(m_commandBuffers[imageIndex]); //Bind model that contains vertex data
-        m_model->Draw(m_commandBuffers[imageIndex]); //Draw
+
+        for (int j = 0; j < 4; ++j) 
+        {
+            SimplePushConstantData push{};
+            push.offset = { -0.5f + frame * 0.0002f, -0.4f + j * 0.25f };
+            push.color = {0.0f, 0.0f, 0.2f + 0.2f * j};
+
+            vkCmdPushConstants(m_commandBuffers[imageIndex], m_pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
+            m_model->Draw(m_commandBuffers[imageIndex]); //Draw
+
+        }
+
 
         vkCmdEndRenderPass(m_commandBuffers[imageIndex]);
         if (vkEndCommandBuffer(m_commandBuffers[imageIndex]) != VK_SUCCESS)
